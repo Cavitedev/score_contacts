dd3590315b6a702a43756658877a079b85d2ec05
Carlos Víllora Tercero <carlosvt@unicarlos.com>
2020-09-02 09:23:42 +0200

    Add company to bloc change

---
+++ lib/domain/features/user/contacts_data/properties/company.freezed.dart
@@ -0,0 +1,137 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies
+
+part of 'company.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+class _$CompanyTearOff {
+  const _$CompanyTearOff();
+
+// ignore: unused_element
+  _Company call({String name, String title}) {
+    return _Company(
+      name: name,
+      title: title,
+    );
+  }
+}
+
+// ignore: unused_element
+const $Company = _$CompanyTearOff();
+
+mixin _$Company {
+  String get name;
+
+  String get title;
+
+  $CompanyCopyWith<Company> get copyWith;
+}
+
+abstract class $CompanyCopyWith<$Res> {
+  factory $CompanyCopyWith(Company value, $Res Function(Company) then) =
+      _$CompanyCopyWithImpl<$Res>;
+
+  $Res call({String name, String title});
+}
+
+class _$CompanyCopyWithImpl<$Res> implements $CompanyCopyWith<$Res> {
+  _$CompanyCopyWithImpl(this._value, this._then);
+
+  final Company _value;
+
+  // ignore: unused_field
+  final $Res Function(Company) _then;
+
+  @override
+  $Res call({
+    Object name = freezed,
+    Object title = freezed,
+  }) {
+    return _then(_value.copyWith(
+      name: name == freezed ? _value.name : name as String,
+      title: title == freezed ? _value.title : title as String,
+    ));
+  }
+}
+
+abstract class _$CompanyCopyWith<$Res> implements $CompanyCopyWith<$Res> {
+  factory _$CompanyCopyWith(_Company value, $Res Function(_Company) then) =
+      __$CompanyCopyWithImpl<$Res>;
+
+  @override
+  $Res call({String name, String title});
+}
+
+class __$CompanyCopyWithImpl<$Res> extends _$CompanyCopyWithImpl<$Res>
+    implements _$CompanyCopyWith<$Res> {
+  __$CompanyCopyWithImpl(_Company _value, $Res Function(_Company) _then)
+      : super(_value, (v) => _then(v as _Company));
+
+  @override
+  _Company get _value => super._value as _Company;
+
+  @override
+  $Res call({
+    Object name = freezed,
+    Object title = freezed,
+  }) {
+    return _then(_Company(
+      name: name == freezed ? _value.name : name as String,
+      title: title == freezed ? _value.title : title as String,
+    ));
+  }
+}
+
+class _$_Company extends _Company {
+  const _$_Company({this.name, this.title}) : super._();
+
+  @override
+  final String name;
+  @override
+  final String title;
+
+  @override
+  String toString() {
+    return 'Company(name: $name, title: $title)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _Company &&
+            (identical(other.name, name) ||
+                const DeepCollectionEquality().equals(other.name, name)) &&
+            (identical(other.title, title) ||
+                const DeepCollectionEquality().equals(other.title, title)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(name) ^
+      const DeepCollectionEquality().hash(title);
+
+  @override
+  _$CompanyCopyWith<_Company> get copyWith =>
+      __$CompanyCopyWithImpl<_Company>(this, _$identity);
+}
+
+abstract class _Company extends Company {
+  const _Company._() : super._();
+
+  const factory _Company({String name, String title}) = _$_Company;
+
+  @override
+  String get name;
+
+  @override
+  String get title;
+
+  @override
+  _$CompanyCopyWith<_Company> get copyWith;
+}
---
+++ lib/domain/features/user/contacts_data/properties/names/name_data.freezed.dart
@@ -0,0 +1,265 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies
+
+part of 'name_data.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+class _$NameDataTearOff {
+  const _$NameDataTearOff();
+
+// ignore: unused_element
+  _NameData call(
+      {PhoneticNames phoneticNames,
+      String firstName,
+      String nickname,
+      String namePrefix,
+      String middleName,
+      String surnames,
+      String nameSuffix}) {
+    return _NameData(
+      phoneticNames: phoneticNames,
+      firstName: firstName,
+      nickname: nickname,
+      namePrefix: namePrefix,
+      middleName: middleName,
+      surnames: surnames,
+      nameSuffix: nameSuffix,
+    );
+  }
+}
+
+// ignore: unused_element
+const $NameData = _$NameDataTearOff();
+
+mixin _$NameData {
+  PhoneticNames get phoneticNames;
+
+  String get firstName;
+
+  String get nickname;
+
+  String get namePrefix;
+
+  String get middleName;
+
+  String get surnames;
+
+  String get nameSuffix;
+
+  $NameDataCopyWith<NameData> get copyWith;
+}
+
+abstract class $NameDataCopyWith<$Res> {
+  factory $NameDataCopyWith(NameData value, $Res Function(NameData) then) =
+      _$NameDataCopyWithImpl<$Res>;
+
+  $Res call(
+      {PhoneticNames phoneticNames,
+      String firstName,
+      String nickname,
+      String namePrefix,
+      String middleName,
+      String surnames,
+      String nameSuffix});
+}
+
+class _$NameDataCopyWithImpl<$Res> implements $NameDataCopyWith<$Res> {
+  _$NameDataCopyWithImpl(this._value, this._then);
+
+  final NameData _value;
+
+  // ignore: unused_field
+  final $Res Function(NameData) _then;
+
+  @override
+  $Res call({
+    Object phoneticNames = freezed,
+    Object firstName = freezed,
+    Object nickname = freezed,
+    Object namePrefix = freezed,
+    Object middleName = freezed,
+    Object surnames = freezed,
+    Object nameSuffix = freezed,
+  }) {
+    return _then(_value.copyWith(
+      phoneticNames: phoneticNames == freezed
+          ? _value.phoneticNames
+          : phoneticNames as PhoneticNames,
+      firstName: firstName == freezed ? _value.firstName : firstName as String,
+      nickname: nickname == freezed ? _value.nickname : nickname as String,
+      namePrefix:
+          namePrefix == freezed ? _value.namePrefix : namePrefix as String,
+      middleName:
+          middleName == freezed ? _value.middleName : middleName as String,
+      surnames: surnames == freezed ? _value.surnames : surnames as String,
+      nameSuffix:
+          nameSuffix == freezed ? _value.nameSuffix : nameSuffix as String,
+    ));
+  }
+}
+
+abstract class _$NameDataCopyWith<$Res> implements $NameDataCopyWith<$Res> {
+  factory _$NameDataCopyWith(_NameData value, $Res Function(_NameData) then) =
+      __$NameDataCopyWithImpl<$Res>;
+
+  @override
+  $Res call(
+      {PhoneticNames phoneticNames,
+      String firstName,
+      String nickname,
+      String namePrefix,
+      String middleName,
+      String surnames,
+      String nameSuffix});
+}
+
+class __$NameDataCopyWithImpl<$Res> extends _$NameDataCopyWithImpl<$Res>
+    implements _$NameDataCopyWith<$Res> {
+  __$NameDataCopyWithImpl(_NameData _value, $Res Function(_NameData) _then)
+      : super(_value, (v) => _then(v as _NameData));
+
+  @override
+  _NameData get _value => super._value as _NameData;
+
+  @override
+  $Res call({
+    Object phoneticNames = freezed,
+    Object firstName = freezed,
+    Object nickname = freezed,
+    Object namePrefix = freezed,
+    Object middleName = freezed,
+    Object surnames = freezed,
+    Object nameSuffix = freezed,
+  }) {
+    return _then(_NameData(
+      phoneticNames: phoneticNames == freezed
+          ? _value.phoneticNames
+          : phoneticNames as PhoneticNames,
+      firstName: firstName == freezed ? _value.firstName : firstName as String,
+      nickname: nickname == freezed ? _value.nickname : nickname as String,
+      namePrefix:
+          namePrefix == freezed ? _value.namePrefix : namePrefix as String,
+      middleName:
+          middleName == freezed ? _value.middleName : middleName as String,
+      surnames: surnames == freezed ? _value.surnames : surnames as String,
+      nameSuffix:
+          nameSuffix == freezed ? _value.nameSuffix : nameSuffix as String,
+    ));
+  }
+}
+
+class _$_NameData extends _NameData {
+  const _$_NameData(
+      {this.phoneticNames,
+      this.firstName,
+      this.nickname,
+      this.namePrefix,
+      this.middleName,
+      this.surnames,
+      this.nameSuffix})
+      : super._();
+
+  @override
+  final PhoneticNames phoneticNames;
+  @override
+  final String firstName;
+  @override
+  final String nickname;
+  @override
+  final String namePrefix;
+  @override
+  final String middleName;
+  @override
+  final String surnames;
+  @override
+  final String nameSuffix;
+
+  @override
+  String toString() {
+    return 'NameData(phoneticNames: $phoneticNames, firstName: $firstName, nickname: $nickname, namePrefix: $namePrefix, middleName: $middleName, surnames: $surnames, nameSuffix: $nameSuffix)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _NameData &&
+            (identical(other.phoneticNames, phoneticNames) ||
+                const DeepCollectionEquality()
+                    .equals(other.phoneticNames, phoneticNames)) &&
+            (identical(other.firstName, firstName) ||
+                const DeepCollectionEquality()
+                    .equals(other.firstName, firstName)) &&
+            (identical(other.nickname, nickname) ||
+                const DeepCollectionEquality()
+                    .equals(other.nickname, nickname)) &&
+            (identical(other.namePrefix, namePrefix) ||
+                const DeepCollectionEquality()
+                    .equals(other.namePrefix, namePrefix)) &&
+            (identical(other.middleName, middleName) ||
+                const DeepCollectionEquality()
+                    .equals(other.middleName, middleName)) &&
+            (identical(other.surnames, surnames) ||
+                const DeepCollectionEquality()
+                    .equals(other.surnames, surnames)) &&
+            (identical(other.nameSuffix, nameSuffix) ||
+                const DeepCollectionEquality()
+                    .equals(other.nameSuffix, nameSuffix)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(phoneticNames) ^
+      const DeepCollectionEquality().hash(firstName) ^
+      const DeepCollectionEquality().hash(nickname) ^
+      const DeepCollectionEquality().hash(namePrefix) ^
+      const DeepCollectionEquality().hash(middleName) ^
+      const DeepCollectionEquality().hash(surnames) ^
+      const DeepCollectionEquality().hash(nameSuffix);
+
+  @override
+  _$NameDataCopyWith<_NameData> get copyWith =>
+      __$NameDataCopyWithImpl<_NameData>(this, _$identity);
+}
+
+abstract class _NameData extends NameData {
+  const _NameData._() : super._();
+
+  const factory _NameData(
+      {PhoneticNames phoneticNames,
+      String firstName,
+      String nickname,
+      String namePrefix,
+      String middleName,
+      String surnames,
+      String nameSuffix}) = _$_NameData;
+
+  @override
+  PhoneticNames get phoneticNames;
+
+  @override
+  String get firstName;
+
+  @override
+  String get nickname;
+
+  @override
+  String get namePrefix;
+
+  @override
+  String get middleName;
+
+  @override
+  String get surnames;
+
+  @override
+  String get nameSuffix;
+
+  @override
+  _$NameDataCopyWith<_NameData> get copyWith;
+}
