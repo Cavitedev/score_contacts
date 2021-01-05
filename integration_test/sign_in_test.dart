2914f868f26ec10db0aaefb80f1b5b7b26e20b42
Carlos Víllora Tercero <carlosvt@unicarlos.com>
2021-01-05 13:03:30 +0100

    Add android support for integration tests

--- integration_test/sign_in_test.dart
+++ integration_test/sign_in_test.dart
@@ -1,15 +1,15 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:scorecontacts/main.dart';
+import 'package:scorecontacts/main.dart' as m;
 
 void main(){
 
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets("failing test example", (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
-
-    expect(find.byType(MyApp), findsOneWidget);
+  testWidgets("Initialize opens MyApp", (WidgetTester tester) async {
+    await m.main();
+    await tester.pumpAndSettle();
+    expect(find.byType(m.MyApp), findsOneWidget);
 
   });
 
