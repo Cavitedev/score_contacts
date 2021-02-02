9693f09c9391a3fbb96a064cd9696f6c00e29c45
Carlos Víllora Tercero <carlosvt@unicarlos.com>
2021-02-02 15:22:00 +0100

    :zap: Moving Firebase initialize to bloc. So it loads faster

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -15,7 +14,6 @@ import 'core/app_localization.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   configureInjection(Environment.prod);
-  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
