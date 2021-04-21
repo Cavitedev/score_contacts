f5814e26911919cc9234447bb0a04b787604c1df
Carlos Víllora Tercero <carlosvt@unicarlos.com>
2021-04-21 20:17:27 +0200

    :lipstick: Fixing some colors

--- lib/theme_manager.dart
+++ lib/theme_manager.dart
@@ -3,7 +3,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 ThemeData lightTheme = ThemeData.light().copyWith(
-    brightness: Brightness.light,
+  brightness: Brightness.light,
   scaffoldBackgroundColor: const Color.fromRGBO(250, 250, 250, 1),
   dialogBackgroundColor: const Color.fromRGBO(245, 245, 245, 1),
   primaryColor: Colors.blue[800],
@@ -24,11 +24,9 @@ ThemeData lightTheme = ThemeData.light().copyWith(
   highlightColor: Colors.blueAccent[700],
   focusColor: Colors.blueAccent[400],
   textSelectionTheme: TextSelectionThemeData(
-    cursorColor: Colors.blue[700],
-    selectionColor: Colors.white,
-    selectionHandleColor: Colors.blue[600]
-  ),
-
+      cursorColor: Colors.blue[700],
+      selectionColor: Colors.white,
+      selectionHandleColor: Colors.blue[600]),
   disabledColor: Colors.grey[500],
   inputDecorationTheme: const InputDecorationTheme(
     border: InputBorder.none,
@@ -39,12 +37,12 @@ ThemeData lightTheme = ThemeData.light().copyWith(
       headline3: const TextStyle(fontSize: 26, color: Colors.white),
       headline4: const TextStyle(fontSize: 24, color: Colors.black),
       headline5: const TextStyle(fontSize: 16, color: Colors.black),
-      headline6: const TextStyle(
-          fontSize: 14, letterSpacing: .5, color: Colors.black),
-      subtitle1: const TextStyle(
-          fontSize: 16, letterSpacing: .5, color: Colors.black),
-      subtitle2: const TextStyle(
-          fontSize: 14, letterSpacing: .5, color: Colors.black),
+      headline6:
+          const TextStyle(fontSize: 14, letterSpacing: .5, color: Colors.black),
+      subtitle1:
+          const TextStyle(fontSize: 16, letterSpacing: .5, color: Colors.black),
+      subtitle2:
+          const TextStyle(fontSize: 14, letterSpacing: .5, color: Colors.black),
       caption: TextStyle(fontSize: 15, color: Colors.grey[700]),
       button: TextStyle(fontSize: 20, color: Colors.blueAccent[400])),
   visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -68,36 +66,28 @@ ThemeData darkTheme = ThemeData.dark().copyWith(
   ),
   dividerColor: const Color.fromRGBO(20, 20, 20, 1),
   highlightColor: Colors.blueAccent[700],
-  focusColor: Colors.blue[50],
-
-    textSelectionTheme: TextSelectionThemeData(
-    cursorColor: Colors.blue[700],
-    selectionColor: const Color.fromRGBO(40, 40, 40, 1),
-    selectionHandleColor: Colors.blue[600]
-  ),
-
+  focusColor: Colors.grey[600],
+  textSelectionTheme: TextSelectionThemeData(
+      cursorColor: Colors.blue[700],
+      selectionColor: const Color.fromRGBO(40, 40, 40, 1),
+      selectionHandleColor: Colors.blue[600]),
   disabledColor: Colors.grey[500],
   inputDecorationTheme: const InputDecorationTheme(
     border: InputBorder.none,
   ),
   iconTheme: const IconThemeData(color: Colors.grey, size: 32),
   textTheme: TextTheme(
-
       headline2: const TextStyle(
           fontSize: 30, fontWeight: FontWeight.w500, color: Colors.black),
       headline3: const TextStyle(fontSize: 26, color: Colors.white),
       headline4: const TextStyle(fontSize: 24, color: Colors.white),
-      headline5: const TextStyle(
-        fontSize: 16,
-        color: Colors.white
-      ),
-      headline6: const TextStyle(fontSize: 14, letterSpacing: .5, color: Colors.white),
-      subtitle1: const TextStyle(fontSize: 16, letterSpacing: .5, color: Colors.white),
-      subtitle2: const TextStyle(
-        fontSize: 14,
-        letterSpacing: .5,
-        color: Colors.white
-      ),
+      headline5: const TextStyle(fontSize: 16, color: Colors.white),
+      headline6:
+          const TextStyle(fontSize: 14, letterSpacing: .5, color: Colors.white),
+      subtitle1:
+          const TextStyle(fontSize: 16, letterSpacing: .5, color: Colors.white),
+      subtitle2:
+          const TextStyle(fontSize: 14, letterSpacing: .5, color: Colors.white),
       caption: TextStyle(fontSize: 15, color: Colors.grey[500]),
       button: TextStyle(fontSize: 20, color: Colors.blue[50])),
   visualDensity: VisualDensity.adaptivePlatformDensity,
