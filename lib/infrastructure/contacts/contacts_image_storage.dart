e8a9607388a71f6260cc54e44028dcaa8edf5ebe
Carlos Víllora Tercero <carlosvt@unicarlos.com>
2021-05-09 13:40:38 +0200

    :recycle: contacts_repository.dart

---
+++ lib/infrastructure/contacts/contacts_image_storage.dart
@@ -0,0 +1,73 @@
+import 'dart:typed_data';
+
+import 'package:dartz/dartz.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:injectable/injectable.dart';
+import 'package:scorecontacts/domain/auth/i_auth_facade.dart';
+import 'package:scorecontacts/domain/user/contacts_data/contact.dart';
+import 'package:scorecontacts/domain/user/contacts_data/contacts_failure.dart';
+import 'package:scorecontacts/infrastructure/core/firebase_user_helper.dart';
+import 'package:scorecontacts/infrastructure/firebase_core/codes.dart';
+
+import 'contact_dtos.dart';
+
+@LazySingleton()
+class ContactsImageStorage {
+  final FirebaseStorage firestorage;
+  final IAuthFacade authFacade;
+
+  ContactsImageStorage(this.firestorage, this.authFacade);
+
+  Reference getFirestorageRef(Contact contact) {
+    final String userId = authFacade.getUserOrCrash().uid.value;
+    final Reference storageRef = firestorage
+        .ref("Users/$userId/contacts/${contact.id.value}/avatar.jpg");
+    return storageRef;
+  }
+
+  Future<Either<ContactsFailure, String?>> uploadImage(Contact contact) async {
+    final PickedFile? imageFile = contact.contactImage?.file;
+    if (imageFile == null) {
+      return right(null);
+    }
+    final Reference storageRef = getFirestorageRef(contact);
+    final Uint8List imageBytes = await imageFile.readAsBytes();
+    final UploadTask uploadTask = storageRef.putData(imageBytes);
+    try {
+      await uploadTask.whenComplete(() {});
+      final url = await uploadTask.snapshot.ref.getDownloadURL();
+      return right(url);
+    } catch (e) {
+      if (e is FirebaseException && e.code == UNATHORIZED) {
+        return left(const ContactsFailure.insufficientPermissions());
+      }
+      rethrow;
+    }
+  }
+
+  Future<Either<ContactsFailure, ContactDTO>> updateImageOnDTO(
+      Contact contact, ContactDTO contactDTO) async {
+    final Either<ContactsFailure, String?> eitherImageUrl =
+        await uploadImage(contact);
+    return eitherImageUrl.fold(
+      (f) => left(f),
+      (imageUrl) {
+        if (imageUrl != null) {
+          return right(contactDTO.copyWith(imageUrl: imageUrl));
+        }
+
+        return right(contactDTO);
+      },
+    );
+  }
+
+  Future<void> deleteImage(Contact contact) {
+    if (contact.contactImage == null) {
+      return Future.value(null);
+    }
+    final Reference storageRef = getFirestorageRef(contact);
+
+    return storageRef.delete();
+  }
+}
--- lib/infrastructure/contacts/contacts_repository.dart
+++ lib/infrastructure/contacts/contacts_repository.dart
@@ -1,27 +1,22 @@
-import 'dart:typed_data';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/services.dart';
-import 'package:image_picker/image_picker.dart';
 import 'package:injectable/injectable.dart';
 import 'package:rxdart/rxdart.dart';
-import 'package:scorecontacts/domain/auth/i_auth_facade.dart';
 import 'package:scorecontacts/domain/user/contacts_data/contact.dart';
 import 'package:scorecontacts/domain/user/contacts_data/contacts_failure.dart';
 import 'package:scorecontacts/domain/user/contacts_data/i_contact_repository.dart';
 import 'package:scorecontacts/infrastructure/contacts/contact_dtos.dart';
+import 'package:scorecontacts/infrastructure/contacts/contacts_image_storage.dart';
 import 'package:scorecontacts/infrastructure/core/firebase_user_helper.dart';
 import 'package:scorecontacts/infrastructure/firebase_core/codes.dart';
 
 @LazySingleton(as: IContactsRepository)
 class ContactsRepository implements IContactsRepository {
   final FirebaseFirestore firestore;
-  final FirebaseStorage firestorage;
-  final IAuthFacade authFacade;
+  final ContactsImageStorage imageStorage;
 
-  ContactsRepository(this.firestore, this.firestorage, this.authFacade);
+  ContactsRepository(this.firestore, this.imageStorage);
 
   //CRUD
   @override
@@ -32,28 +27,22 @@ class ContactsRepository implements IContactsRepository {
         (r) => right(unit),
       );
     } catch (e) {
-      if (e is PlatformException &&
-          (e.message!.contains(PERMISSIONDENIEDCODE) ||
-              e.code == UNATHORIZED)) {
-        return left(const ContactsFailure.insufficientPermissions());
-      } else {
-        return left(const ContactsFailure.unexpected());
-      }
+      return left(_handleException(e));
     }
   }
 
   @override
   Future<Either<ContactsFailure, Unit>> createContactList(
       List<Contact> contactList) async {
-    return _performingBatch<Either<ContactsFailure, Unit>, List<Contact>>(
+    return _performBatch<Either<ContactsFailure, Unit>, List<Contact>>(
         contactList, _createContactsBatch);
   }
 
   Future<Either<ContactsFailure, void>> _createContact(Contact contact) async {
     final CollectionReference contacts =
         firestore.userDocument().contactsCollection;
-    ContactDTO contactDTO = ContactDTO.fromDomain(contact);
-    return (await _updateImageOnDTO(contact, contactDTO)).fold(
+    final ContactDTO contactDTO = ContactDTO.fromDomain(contact);
+    return (await imageStorage.updateImageOnDTO(contact, contactDTO)).fold(
       (f) => left(f),
       (contactDtoRet) =>
           right(contacts.doc(contactDtoRet.id).set(contactDtoRet.toJson())),
@@ -68,7 +57,7 @@ class ContactsRepository implements IContactsRepository {
     ContactsFailure? failure;
     for (final Contact contact in contactList) {
       ContactDTO contactDTO = ContactDTO.fromDomain(contact);
-      (await _updateImageOnDTO(contact, contactDTO)).fold(
+      (await imageStorage.updateImageOnDTO(contact, contactDTO)).fold(
         (l) => failure = l,
         (contactDtoRet) => contactDTO = contactDtoRet,
       );
@@ -97,16 +86,12 @@ class ContactsRepository implements IContactsRepository {
     final CollectionReference contacts =
         firestore.userDocument().contactsCollection;
     yield* contacts
-        .snapshots(includeMetadataChanges: true)
+        .snapshots()
         .map((snapshot) => right<ContactsFailure, List<Contact>>(snapshot.docs
             .map((doc) => ContactDTO.fromFirestore(doc).toDomain())
             .toList()))
         .onErrorReturnWith((e) {
-      if (e is PlatformException && e.message!.contains(PERMISSIONDENIEDCODE)) {
-        return left(const ContactsFailure.insufficientPermissions());
-      } else {
-        return left(const ContactsFailure.unexpected());
-      }
+      return left(_handleException(e));
     });
   }
 
@@ -116,8 +101,8 @@ class ContactsRepository implements IContactsRepository {
         firestore.userDocument().contactsCollection;
     final ContactDTO contactDTO = ContactDTO.fromDomain(contact);
 
-    Either<ContactsFailure, ContactDTO> eitherUpdatedDto =
-        await _updateImageOnDTO(contact, contactDTO);
+    final Either<ContactsFailure, ContactDTO> eitherUpdatedDto =
+        await imageStorage.updateImageOnDTO(contact, contactDTO);
 
     return eitherUpdatedDto.fold(
       (f) => left(f),
@@ -126,15 +111,7 @@ class ContactsRepository implements IContactsRepository {
           await contacts.doc(contactDTORet.id).update(contactDTORet.toJson());
           return right(unit);
         } catch (e) {
-          if (e is PlatformException &&
-              e.message!.contains(PERMISSIONDENIEDCODE)) {
-            return left(const ContactsFailure.insufficientPermissions());
-          } else if (e is PlatformException &&
-              e.message!.contains(NOTFOUNDCODE)) {
-            return left(const ContactsFailure.notFound());
-          } else {
-            return left(const ContactsFailure.unexpected());
-          }
+          return left(_handleException(e));
         }
       },
     );
@@ -144,7 +121,7 @@ class ContactsRepository implements IContactsRepository {
   Future<Either<ContactsFailure, Unit>> deleteContact(Contact contact) async {
     try {
       try {
-        await _deleteImage(contact);
+        await imageStorage.deleteImage(contact);
       } catch (e) {
         return left(const ContactsFailure.unexpected());
       }
@@ -153,20 +130,14 @@ class ContactsRepository implements IContactsRepository {
       await contacts.doc(contact.id.value).delete();
       return right(unit);
     } catch (e) {
-      if (e is PlatformException && e.message!.contains(PERMISSIONDENIEDCODE)) {
-        return left(const ContactsFailure.insufficientPermissions());
-      } else if (e is PlatformException && e.message!.contains(NOTFOUNDCODE)) {
-        return left(const ContactsFailure.notFound());
-      } else {
-        return left(const ContactsFailure.unexpected());
-      }
+      return left(_handleException(e));
     }
   }
 
   @override
   Future<Either<ContactsFailure, Unit>> deleteContactList(
       List<Contact> contactList) {
-    return _performingBatch(contactList, _deleteContactsBatch);
+    return _performBatch(contactList, _deleteContactsBatch);
   }
 
   Future<Either<ContactsFailure, Unit>> _deleteContactsBatch(
@@ -176,26 +147,21 @@ class ContactsRepository implements IContactsRepository {
         firestore.userDocument().contactsCollection;
     for (final Contact contact in contactList) {
       try {
-        await _deleteImage(contact);
+        await imageStorage.deleteImage(contact);
       } catch (e) {
-        return left(const ContactsFailure.unexpected());
+        return left(_handleException(e));
       }
       batch.delete(contacts.doc(contact.id.value));
     }
-    Either<ContactsFailure, Unit>? returnValue;
-    await batch.commit().catchError((e) {
-      if (e is PlatformException && e.message!.contains(PERMISSIONDENIEDCODE)) {
-        returnValue = left(const ContactsFailure.insufficientPermissions());
-      } else if (e is PlatformException && e.message!.contains(NOTFOUNDCODE)) {
-        returnValue = left(const ContactsFailure.notFound());
-      } else {
-        returnValue = left(const ContactsFailure.unexpected());
-      }
-    });
-    return returnValue ?? right(unit);
+    try {
+      await batch.commit();
+      return right(unit);
+    } catch (e) {
+      return left(_handleException(e));
+    }
   }
 
-  Future<R> _performingBatch<R extends Either, P extends List>(
+  Future<R> _performBatch<R extends Either, P extends List>(
       P batchList, Future<R> Function(P) batchOperation) async {
     const int range = 200;
 
@@ -207,59 +173,17 @@ class ContactsRepository implements IContactsRepository {
         return batchOperation(batchList);
       }
     }
-
     return batchOperation(batchList);
   }
+}
 
-  Reference _getFirestorageRef(Contact contact) {
-    final String userId = authFacade.getUserOrCrash().uid.value;
-    final Reference storageRef = firestorage
-        .ref("Users/$userId/contacts/${contact.id.value}/avatar.jpg");
-    return storageRef;
-  }
-
-  Future<Either<ContactsFailure, String?>> _uploadImage(Contact contact) async {
-    final PickedFile? imageFile = contact.contactImage?.file;
-    if (imageFile == null) {
-      return right(null);
-    }
-    final Reference storageRef = _getFirestorageRef(contact);
-    final Uint8List imageBytes = await imageFile.readAsBytes();
-    final UploadTask uploadTask = storageRef.putData(imageBytes);
-    try {
-      await uploadTask.whenComplete(() {});
-      final url = await uploadTask.snapshot.ref.getDownloadURL();
-      return right(url);
-    } catch (e) {
-      if (e is FirebaseException && e.code == UNATHORIZED) {
-        return left(const ContactsFailure.insufficientPermissions());
-      }
-      rethrow;
-    }
-  }
-
-  Future<Either<ContactsFailure, ContactDTO>> _updateImageOnDTO(
-      Contact contact, ContactDTO contactDTO) async {
-    Either<ContactsFailure, String?> eitherImageUrl =
-        await _uploadImage(contact);
-    return eitherImageUrl.fold(
-      (f) => left(f),
-      (imageUrl) {
-        if (imageUrl != null) {
-          return right(contactDTO.copyWith(imageUrl: imageUrl));
-        }
-
-        return right(contactDTO);
-      },
-    );
-  }
-
-  Future<void> _deleteImage(Contact contact) {
-    if (contact.contactImage == null) {
-      return Future.value(null);
-    }
-    final Reference storageRef = _getFirestorageRef(contact);
-
-    return storageRef.delete();
+ContactsFailure _handleException(Object e) {
+  if (e is PlatformException &&
+      (e.message!.contains(PERMISSIONDENIEDCODE) || e.code == UNATHORIZED)) {
+    return const ContactsFailure.insufficientPermissions();
+  } else if (e is PlatformException && e.message!.contains(NOTFOUNDCODE)) {
+    return const ContactsFailure.notFound();
+  } else {
+    return const ContactsFailure.unexpected();
   }
 }
