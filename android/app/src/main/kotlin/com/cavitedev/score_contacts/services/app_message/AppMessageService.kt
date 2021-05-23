37a7c2f50e60b100568a66ff50ab9691fd2fc13d
Carlos Víllora Tercero <carlosvt@unicarlos.com>
2021-05-23 18:36:54 +0200

    :construction: Working on check contact has whastapp

--- android/app/src/main/kotlin/com/cavitedev/score_contacts/services/app_message/AppMessageService.kt
+++ android/app/src/main/kotlin/com/cavitedev/score_contacts/services/app_message/AppMessageService.kt
@@ -1,8 +1,12 @@
 package com.cavitedev.score_contacts.services.app_message
 
 import android.content.ComponentName
+import android.content.ContentResolver
 import android.content.Context
 import android.content.Intent
+import android.database.Cursor
+import android.database.CursorJoiner
+import android.provider.ContactsContract
 import android.telephony.PhoneNumberUtils
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
@@ -23,6 +27,7 @@ object AppMessageService {
                     val args: List<String> = call.arguments as List<String>
                     val num = args[0]
                     val app = args[1]
+                    hasWhatsapp(context, num)
                     sendMessage(context, num, app)
                     result.success(null)
                 } catch (e: Exception) {
@@ -36,6 +41,74 @@ object AppMessageService {
         }
 
     }
+
+//https://stackoverflow.com/a/35453979/14559140
+    fun hasWhatsapp(context: Context, phoneNum: String): Boolean {
+
+//        val cr: ContentResolver = context.contentResolver
+//
+//        val contactCursor: Cursor? = cr.query(
+//                ContactsContract.RawContacts.CONTENT_URI,
+//                arrayOf(ContactsContract.RawContacts._ID, ContactsContract.RawContacts.CONTACT_ID),
+//                ContactsContract.RawContacts.ACCOUNT_TYPE + "= ?", arrayOf("com.whatsapp"),
+//                null)
+//
+//
+//        if (contactCursor != null) {
+//            if (contactCursor.count > 0) {
+//                val whatsAppContactCursor: Cursor? = cr.query(
+//                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI, arrayOf(ContactsContract.CommonDataKinds.Phone.CONTACT_ID,
+//                        ContactsContract.CommonDataKinds.Phone.NUMBER),
+//                        "CAST(${ContactsContract.CommonDataKinds.Phone.NUMBER} AS INTEGER) LIKE  ?", arrayOf("%640232416"), null)
+//                val join = CursorJoiner(contactCursor, arrayOf(ContactsContract.RawContacts.CONTACT_ID), whatsAppContactCursor, arrayOf(ContactsContract.CommonDataKinds.Phone.CONTACT_ID))
+//
+////                (whatsAppContactCursor.getString(whatsAppContactCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)))
+//
+//
+//            }
+//        }
+
+//        val whatsAppContactCursor: Cursor? = cr.query(
+//        ContactsContract.CommonDataKinds.Phone.CONTENT_URI, arrayOf(ContactsContract.CommonDataKinds.Phone.CONTACT_ID,
+//        ContactsContract.CommonDataKinds.Phone.NUMBER,
+//        ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME),
+//        "CAST(${ContactsContract.CommonDataKinds.Phone.NUMBER} AS INTEGER) LIKE  ?", arrayOf("%640232416"), null)
+//val join = CursorJoiner(contactCursor, arrayOf(ContactsContract.RawContacts.CONTACT_ID), whatsAppContactCursor, arrayOf(ContactsContract.CommonDataKinds.Phone.CONTACT_ID))
+//
+//(whatsAppContactCursor.getString(whatsAppContactCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)))
+//
+
+
+//        showLogI(TAG, " WhatsApp contact size :  " + myWhatsappContacts.size())
+
+//        val hasWhatsApp: Boolean
+////        val projection = arrayOf<String>(ContactsContract.RawContacts._ID)
+//        val projection = arrayOf(ContactsContract.CommonDataKinds.Phone.CONTACT_ID)
+////        val selection: String = "${ContactsContract.CommonDataKinds.Phone.CONTENT_URI} = ? AND account_type IN (?)"
+//        val selection: String = "account_type IN (?)"
+////        val selectionArgs = arrayOf(phoneNum, "com.whatsapp")
+//        val selectionArgs = arrayOf("com.whatsapp")
+//        val cursor: Cursor? = context.contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, projection, selection, selectionArgs, null)
+//        if (cursor != null) {
+//            hasWhatsApp = cursor.moveToNext()
+//            cursor.close()
+//        }else{
+//            hasWhatsApp = false
+//        }
+//        return hasWhatsApp
+
+//        val projection = arrayOf<String>(RawContacts._ID)
+//        val selection = ContactsContract.Data.CONTACT_ID + " = ? AND account_type IN (?)"
+//        val selectionArgs = arrayOf("THE_CONTACT_DEVICE_ID", "com.whatsapp")
+//        val cursor: Cursor = activity.getContentResolver().query(RawContacts.CONTENT_URI, projection, selection, selectionArgs, null)
+//        val hasWhatsApp = cursor.moveToNext()
+//        if (hasWhatsApp) {
+//            val rowContactId = cursor.getString(0)
+//        }
+
+        return false
+    }
+
 
     private fun sendMessage(context: Context, phone: String, app: String) {
 
