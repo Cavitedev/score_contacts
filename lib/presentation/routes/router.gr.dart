9271d30db601bfd2a759fb37e57ad501df5e3a0a
Carlos Víllora Tercero <carlosvt@unicarlos.com>
2021-06-11 19:16:10 +0200

    :arrow_up: Update firebase dependencies

--- lib/presentation/routes/router.gr.dart
+++ lib/presentation/routes/router.gr.dart
@@ -7,9 +7,9 @@
 import 'package:auto_route/auto_route.dart' as _i1;
 import 'package:flutter/material.dart' as _i2;
 
-import '../../domain/mention/i_mentionable.dart' as _i12;
+import '../../domain/mention/i_mentionable.dart' as _i13;
 import '../../domain/user/contacts_data/contact.dart' as _i11;
-import '../../domain/user/diary/diary_entry.dart' as _i13;
+import '../../domain/user/diary/diary_entry.dart' as _i12;
 import '../auth/sign_in/sign_in_page.dart' as _i6;
 import '../auth/splash_page.dart' as _i3;
 import '../contacts/add_contacts/add_contact_page.dart' as _i5;
@@ -62,11 +62,9 @@ class Router extends _i1.RootStackRouter {
     AddDiaryPageRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
         routeData: routeData,
         builder: (data) {
-          final args = data.argsAs<AddDiaryPageRouteArgs>();
-          return _i9.AddDiaryPage(
-              key: args.key,
-              mentionableList: args.mentionableList,
-              diaryEntry: args.diaryEntry);
+          final args = data.argsAs<AddDiaryPageRouteArgs>(
+              orElse: () => const AddDiaryPageRouteArgs());
+          return _i9.AddDiaryPage(key: args.key, diaryEntry: args.diaryEntry);
         }),
     ListDiaryEntryPageRoute.name: (routeData) => _i1.MaterialPageX<dynamic>(
         routeData: routeData,
@@ -153,34 +151,25 @@ class ViewContactPageRouteArgs {
 }
 
 class AddDiaryPageRoute extends _i1.PageRouteInfo<AddDiaryPageRouteArgs> {
-  AddDiaryPageRoute(
-      {_i2.Key? key,
-      required List<_i12.IMentionable> mentionableList,
-      _i13.DiaryEntry? diaryEntry})
+  AddDiaryPageRoute({_i2.Key? key, _i12.DiaryEntry? diaryEntry})
       : super(name,
             path: '/add-diary-page',
-            args: AddDiaryPageRouteArgs(
-                key: key,
-                mentionableList: mentionableList,
-                diaryEntry: diaryEntry));
+            args: AddDiaryPageRouteArgs(key: key, diaryEntry: diaryEntry));
 
   static const String name = 'AddDiaryPageRoute';
 }
 
 class AddDiaryPageRouteArgs {
-  const AddDiaryPageRouteArgs(
-      {this.key, required this.mentionableList, this.diaryEntry});
+  const AddDiaryPageRouteArgs({this.key, this.diaryEntry});
 
   final _i2.Key? key;
 
-  final List<_i12.IMentionable> mentionableList;
-
-  final _i13.DiaryEntry? diaryEntry;
+  final _i12.DiaryEntry? diaryEntry;
 }
 
 class ListDiaryEntryPageRoute
     extends _i1.PageRouteInfo<ListDiaryEntryPageRouteArgs> {
-  ListDiaryEntryPageRoute({_i12.IMentionable? mentionable})
+  ListDiaryEntryPageRoute({_i13.IMentionable? mentionable})
       : super(name,
             path: '/list-diary-entry-page',
             args: ListDiaryEntryPageRouteArgs(mentionable: mentionable));
@@ -191,5 +180,5 @@ class ListDiaryEntryPageRoute
 class ListDiaryEntryPageRouteArgs {
   const ListDiaryEntryPageRouteArgs({this.mentionable});
 
-  final _i12.IMentionable? mentionable;
+  final _i13.IMentionable? mentionable;
 }
