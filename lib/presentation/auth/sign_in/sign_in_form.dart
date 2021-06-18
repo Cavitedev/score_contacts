c3df7e2ddb53accf94d2c8feadd6dd57089550b3
Carlos Víllora Tercero <carlosvt@unicarlos.com>
2021-06-18 17:05:36 +0200

    :lipstick: Add Google icon on sign page

--- lib/presentation/auth/sign_in/sign_in_form.dart
+++ lib/presentation/auth/sign_in/sign_in_form.dart
@@ -1,9 +1,11 @@
 import 'package:another_flushbar/flushbar_helper.dart';
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:scorecontacts/application/auth/auth_bloc.dart';
 import 'package:scorecontacts/application/auth/sign_in/bloc.dart';
+import 'package:scorecontacts/core/app_constants.dart';
 import 'package:scorecontacts/core/app_localization.dart';
 import 'package:scorecontacts/presentation/routes/router.gr.dart' as r;
 
@@ -16,7 +18,10 @@ class SignInForm extends StatelessWidget {
           context.read<AuthBloc>().add(const AuthEvent.getUser());
           context.router.popAndPush(const r.ContactListRoute());
         } else if (state is ErrorSignInState) {
-          FlushbarHelper.createError(message: AppLocalization.of(context).translate(state.authFailure.message), duration: const Duration(seconds: 5))
+          FlushbarHelper.createError(
+                  message:
+                      AppLocalization.of(context).translate(state.authFailure.message),
+                  duration: const Duration(seconds: 5))
               .show(context);
         }
       },
@@ -37,20 +42,37 @@ class SignInForm extends StatelessWidget {
               height: 20,
             ),
             if (state is ErrorSignInState) ...[Text(state.authFailure.message)],
-            if (state is SubmittingSignInState) ...[
-              const LinearProgressIndicator(
-                value: null,
-              )
-            ],
+            if (state is SubmittingSignInState) ...[const LinearProgressIndicator()],
             const Spacer(),
-            ElevatedButton.icon(
-              onPressed: () {
-                context.read<SignInBloc>().add(SignInWithGoogle());
-              },
-              icon: Icon(Icons.email, color: Theme.of(context).focusColor),
-              label: Text(
-                AppLocalization.of(context).translate("sign_in_google"),
-                style: Theme.of(context).textTheme.button,
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: Constants.extendedPadding),
+              height: 50,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.all(4)
+                ),
+                onPressed: () {
+                  context.read<SignInBloc>().add(SignInWithGoogle());
+                },
+                child: Row(
+                  children: [
+                    Container(
+                      width: 46,
+                      color: Colors.white,
+                      child: Image.asset(
+                        "assets/icons/brands/google-icon.png",
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 12,
+                    ),
+                    Text(
+                      AppLocalization.of(context).translate("sign_in_google"),
+                      style: Theme.of(context).textTheme.button,
+                    )
+                  ],
+                ),
               ),
             ),
             const SizedBox(
