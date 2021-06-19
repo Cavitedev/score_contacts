6f98635d301af8b102ce9359fff9b6e5bc584e6e
Carlos Víllora Tercero <carlosvt@unicarlos.com>
2021-06-19 17:51:35 +0200

    :lipstick: Add hero tag to animate on Contact page pop

--- lib/presentation/contacts/widgets/contact_circle_avatar.dart
+++ lib/presentation/contacts/widgets/contact_circle_avatar.dart
@@ -22,31 +22,34 @@ class ContactCircleAvatar extends StatelessWidget {
   Widget _buildAvatar(BuildContext context) {
     final String? urlContact = selectionContact.contact.contactImage?.url;
     final double increaseFactor = radius / 20;
-    return CustomCircleAvatar(
-      backgroundColor: selectionContact.isSelected
-          ? Colors.teal[200]
-          : colorGivenName(selectionContact.contact.getName()),
-      image: _doesRequiresImage(urlContact) ? urlContact! : null,
-      name: selectionContact.contact.nameData.toFullName(),
-      radius: radius,
-      child: selectionContact.isSelected
-          ? Icon(
-              Icons.check,
-              color: Colors.black,
-              size: 30 * increaseFactor,
-            )
-          : selectionContact.contact.getDisplayedChar() == ""
-              ? Icon(
-                  Icons.person,
-                  color: Colors.black,
-                  size: 30 * increaseFactor,
-                )
-              : Text(
-                  selectionContact.contact.getDisplayedChar(),
-                  style: Theme.of(context).textTheme.headline2?.copyWith(
-                        fontSize: 30 * increaseFactor,
-                      ),
-                ),
+    return Hero(
+      tag: selectionContact.contact.id.value,
+      child: CustomCircleAvatar(
+        backgroundColor: selectionContact.isSelected
+            ? Colors.teal[200]
+            : colorGivenName(selectionContact.contact.getName()),
+        image: _doesRequiresImage(urlContact) ? urlContact! : null,
+        name: selectionContact.contact.nameData.toFullName(),
+        radius: radius,
+        child: selectionContact.isSelected
+            ? Icon(
+                Icons.check,
+                color: Colors.black,
+                size: 30 * increaseFactor,
+              )
+            : selectionContact.contact.getDisplayedChar() == ""
+                ? Icon(
+                    Icons.person,
+                    color: Colors.black,
+                    size: 30 * increaseFactor,
+                  )
+                : Text(
+                    selectionContact.contact.getDisplayedChar(),
+                    style: Theme.of(context).textTheme.headline2?.copyWith(
+                          fontSize: 30 * increaseFactor,
+                        ),
+                  ),
+      ),
     );
   }
 
